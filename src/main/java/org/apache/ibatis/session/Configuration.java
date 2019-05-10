f170029755d1d384dffb96a698661ff4e0ed3963
张恒强 <zhanghengqiang@jd.com>
2019-05-10 20:59:46 +0800

    Update Configuration.java
    1

--- src/main/java/org/apache/ibatis/session/Configuration.java
+++ src/main/java/org/apache/ibatis/session/Configuration.java
@@ -98,28 +98,107 @@ public class Configuration {
 
   protected Environment environment;
 
+  /**
+   * 允许在嵌套语句中使用分页（RowBounds）。如果允许使用则设置为 false。
+   */
   protected boolean safeRowBoundsEnabled;
+  /**
+   * 允许在嵌套语句中使用分页（RowBounds）。如果允许使用则设置为 false。
+   */
   protected boolean safeResultHandlerEnabled = true;
+  /**
+   * 是否开启自动驼峰命名规则（camel case）映射，即从经典数据库列名 A_COLUMN 到经典 Java 属性名 aColumn 的类似映射。
+   * 自动映射时很管用
+   */
   protected boolean mapUnderscoreToCamelCase;
+  /**
+   * 当开启时，任何方法的调用都会加载该对象的所有属性。 否则，每个属性会按需加载（参考 lazyLoadTriggerMethods)。
+   */
   protected boolean aggressiveLazyLoading;
+  /**
+   * 是否允许单一语句返回多结果集（需要驱动支持）。
+   * 一般是使用存储过程来实现
+   */
   protected boolean multipleResultSetsEnabled = true;
+  /**
+   * 允许 JDBC 支持自动生成主键，需要驱动支持。 如果设置为 true 则这个设置强制使用自动生成主键，尽管一些驱动不能支持但仍可正常工作（比如 Derby）。
+   */
   protected boolean useGeneratedKeys;
+  /**
+   * 使用列标签代替列名。不同的驱动在这方面会有不同的表现，具体可参考相关驱动文档或通过测试这两种不同的模式来观察所用驱动的结果
+   */
   protected boolean useColumnLabel = true;
+  /**
+   * 全局地开启或关闭配置文件中的所有映射器已经配置的任何缓存。
+   */
   protected boolean cacheEnabled = true;
+  /**
+   * 指定当结果集中值为 null 的时候是否调用映射对象的 setter（map 对象时为 put）方法，
+   * 这在依赖于 Map.keySet() 或 null 值初始化的时候比较有用。注意基本类型（int、boolean 等）是不能设置成 null 的。
+   */
   protected boolean callSettersOnNulls;
+  /**
+   * 允许使用方法签名中的名称作为语句参数名称。 为了使用该特性，你的项目必须采用 Java 8 编译，并且加上 -parameters 选项
+   */
   protected boolean useActualParamName = true;
+  /**
+   * 当返回行的所有列都是空时，MyBatis默认返回 null。 当开启这个设置时，MyBatis会返回一个空实例。
+   * 请注意，它也适用于嵌套的结果集 （如集合或关联）。（新增于 3.4.2）
+   */
   protected boolean returnInstanceForEmptyRow;
 
+  /**
+   * 指定 MyBatis 增加到日志名称的前缀。
+   */
   protected String logPrefix;
+  /**
+   * 指定 MyBatis 所用日志的具体实现，未指定时将自动查找。
+   * SLF4J | LOG4J | LOG4J2 | JDK_LOGGING | COMMONS_LOGGING | STDOUT_LOGGING | NO_LOGGING
+   */
   protected Class <? extends Log> logImpl;
+  /**
+   * 指定 VFS 的实现
+   */
   protected Class <? extends VFS> vfsImpl;
+  /**
+   * MyBatis 利用本地缓存机制（Local Cache）防止循环引用（circular references）和加速重复嵌套查询。
+   * 默认值为 SESSION，这种情况下会缓存一个会话中执行的所有查询。
+   * 若设置值为 STATEMENT，本地会话仅用在语句执行上，对相同 SqlSession 的不同调用将不会共享数据。
+   */
   protected LocalCacheScope localCacheScope = LocalCacheScope.SESSION;
+  /**
+   * 当没有为参数提供特定的 JDBC 类型时，为空值指定 JDBC 类型。
+   * 某些驱动需要指定列的 JDBC 类型，多数情况直接用一般类型即可，比如 NULL、VARCHAR 或 OTHER。
+   */
   protected JdbcType jdbcTypeForNull = JdbcType.OTHER;
+  /**
+   * 指定哪个对象的方法触发一次延迟加载
+   */
   protected Set<String> lazyLoadTriggerMethods = new HashSet<>(Arrays.asList("equals", "clone", "hashCode", "toString"));
+  /**
+   * 设置超时时间，它决定驱动等待数据库响应的秒数。
+   */
   protected Integer defaultStatementTimeout;
+  /**
+   * 为驱动的结果集获取数量（fetchSize）设置一个提示值。此参数只可以在查询设置中被覆盖。
+   */
   protected Integer defaultFetchSize;
+  /**
+   * 配置默认的执行器。SIMPLE 就是普通的执行器；REUSE 执行器会重用预处理语句（prepared statements）； BATCH 执行器将重用语句并执行批量更新。
+   */
   protected ExecutorType defaultExecutorType = ExecutorType.SIMPLE;
+  /**
+   * 指定 MyBatis 应如何自动映射列到字段或属性。 NONE 表示取消自动映射；PARTIAL 只会自动映射没有定义嵌套结果集映射的结果集。 FULL 会自动映射任意复杂的结果集（无论是否嵌套）
+   */
   protected AutoMappingBehavior autoMappingBehavior = AutoMappingBehavior.PARTIAL;
+  /**
+   * 指定发现自动映射目标未知列（或者未知属性类型）的行为。
+   * NONE: 不做任何反应
+   * WARNING: 输出提醒日志 ('org.apache.ibatis.session.AutoMappingUnknownColumnBehavior' 的日志等级必须设置为 WARN)
+   * FAILING: 映射失败 (抛出 SqlSessionException)
+   *
+   * 与 {@link #autoMappingBehavior} 配合使用
+   */
   protected AutoMappingUnknownColumnBehavior autoMappingUnknownColumnBehavior = AutoMappingUnknownColumnBehavior.NONE;
 
   protected Properties variables = new Properties();
@@ -127,7 +206,13 @@ public class Configuration {
   protected ObjectFactory objectFactory = new DefaultObjectFactory();
   protected ObjectWrapperFactory objectWrapperFactory = new DefaultObjectWrapperFactory();
 
+  /**
+   * 延迟加载的全局开关。当开启时，所有关联对象都会延迟加载。 特定关联关系中可通过设置 fetchType 属性来覆盖该项的开关状态。
+   */
   protected boolean lazyLoadingEnabled = false;
+  /**
+   * 指定 Mybatis 创建具有延迟加载能力的对象所用到的代理工具。
+   */
   protected ProxyFactory proxyFactory = new JavassistProxyFactory(); // #224 Using internal Javassist instead of OGNL
 
   protected String databaseId;
@@ -135,14 +220,29 @@ public class Configuration {
    * Configuration factory class.
    * Used to create Configuration for loading deserialized unread properties.
    *
+   * 指定一个提供 Configuration 实例的类。 这个被返回的 Configuration 实例用来加载被反序列化对象的延迟加载属性值。 这个类必须包含一个签名为static Configuration getConfiguration() 的方法。（新增于 3.2.3）
+   *
    * @see <a href='https://code.google.com/p/mybatis/issues/detail?id=300'>Issue 300 (google code)</a>
    */
   protected Class<?> configurationFactory;
 
   protected final MapperRegistry mapperRegistry = new MapperRegistry(this);
   protected final InterceptorChain interceptorChain = new InterceptorChain();
+  /**
+   * 指定 Enum 使用的默认 TypeHandler defaultEnumTypeHandler
+   * {@link #setDefaultEnumTypeHandler(Class)}
+   */
   protected final TypeHandlerRegistry typeHandlerRegistry = new TypeHandlerRegistry();
+  /**
+   * 类型别名是为 Java 类型设置一个短的名字。 它只和 XML 配置有关，存在的意义仅在于用来减少类完全限定名的冗余
+   * 也可以指定一个包名，MyBatis 会在包名下面搜索需要的 Java Bean ,每一个在包 domain.blog 中的 Java Bean，在没有注解的情况下，会使用 Bean 的首字母小写的非限定类名来作为它的别名
+   * 若有注解，则别名为其注解值 {@link org.apache.ibatis.type.Alias}
+   */
   protected final TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+  /**
+   * 指定动态 SQL 生成的默认语言。 defaultScriptingLanguage
+   * {@link #setDefaultScriptingLanguage(Class)}
+   */
   protected final LanguageDriverRegistry languageRegistry = new LanguageDriverRegistry();
 
   protected final Map<String, MappedStatement> mappedStatements = new StrictMap<>("Mapped Statements collection");
